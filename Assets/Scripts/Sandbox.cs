b552cdae049db8bb94a45a8aad9a00a4d0ffa1cc
kanais7 <kanais0620@gmail.com>
2021-10-28 16:42:12 +0900

    add reference test

---
+++ Assets/Scripts/Sandbox.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sandbox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(typeof(HogeScript));
+        Debug.Log(typeof(FugaScript));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
